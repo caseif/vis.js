0df4a3f83b6058702867e9f540f3fbfca2465721
mproncace <mproncace@gmail.com>
2015-04-15 20:32:07 -0400

    Put song artist and title in page title

--- index.php
+++ index.php
@@ -11,44 +11,43 @@
 			),
 			'gravity' => array(
 				'file' => 'gravity.mp3',
-				'artist' => 'UMPIRE',
-				'title' => 'GRAVITY',
+				'artist' => 'Umpire',
+				'title' => 'Gravity',
 				'genre' => 'Dubstep'
 			),
 			'pressure' => array(
 				'file' => 'pressure.mp3',
-				'artist' => 'DRAPER',
-				'title' => 'THE PRESSURE (FEAT. LAURA BREHM)',
+				'artist' => 'Draper',
+				'title' => 'Pressure (feat. Laura Brehm)',
 				'genre' => 'EDM'
 			),
 			'equinox' => array(
 				'file' => 'equinox.mp3',
-				'artist' => 'SKRILLEX',
-				'title' => 'FIRST OF THE YEAR (EQUINOX)',
+				'artist' => 'Skrillex',
+				'title' => 'First Of The Year (Equinox)',
 				'genre' => 'Dubstep'
 			),
 			'triumph' => array(
 				'file' => 'triumph.mp3',
 				'artist' => 'WRLD',
-				'title' => 'TRIUMPH',
+				'title' => 'Triumph',
 				'genre' => 'Future Bass'
 			),
 			'friends' => array(
 				'file' => 'friends.mp3',
-				'artist' => 'GRABBITZ',
-				'title' => 'FRIENDS (WITH FAUSTIX)',
+				'artist' => 'Grabbitz',
+				'title' => 'Friends (with Faustix)',
 				'genre' => 'Trap'
 			),
 			'thegirl' => array(
 				'file' => 'thegirl.mp3',
-				'artist' => 'HELLBERG',
-				'title' => 'THE GIRL (FEAT. COZI ZUEHLSDORFF)',
+				'artist' => 'Hellberg',
+				'title' => 'The Girl (feat. Cozi Zuehlsdorff)',
 				'genre' => 'House'
 			)
 		);
 		$song = $songs[isset($_GET['song']) ? $_GET['song'] : array_rand($songs)];
 		?>
-		<title>vis</title>
 		<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 		<link rel="stylesheet" href="vis.css">
 		<script>
@@ -57,13 +56,14 @@
 			var title = '<?php echo $song['title']; ?>';
 			var genre = '<?php echo isset($song['genre']) ? $song['genre'] : 'EDM'; ?>';
 		</script>
+		<title><?php echo $song['artist'].' &mdash; '.$song['title']; ?></title>
 	</head>
 	<body>
 		<div id="hue">Loading moosic, please wait...</div>
 		<div class="content">
 			<canvas id="canvas" style="display: block;"></canvas>
-			<div class="names"><?php echo $song['artist']; ?></div>
-			<div class="title"><?php echo $song['title']; ?></div>
+			<div class="names"><?php echo strtoupper($song['artist']); ?></div>
+			<div class="title"><?php echo strtoupper($song['title']); ?></div>
 		</div>
 		
 		<script>
