88c671f46ec5051dc2aeb8bac991ba9040d6da96
caseif <me@caseif.net>
2015-12-15 18:56:20 -0500

    Disable pause button while music is loading

--- js/vis.js
+++ js/vis.js
@@ -272,22 +272,24 @@ var KEY_P_LOWER = 112;
 
 $(document).keypress(function (event) {
     if (event.which == KEY_P_UPPER || event.which == KEY_P_LOWER) {
-        if (isPlaying) {
-            bufferSource.stop();
-            currentTime += Date.now() - started;
-            velMult = 0;
-        } else {
-            var newSource = context.createBufferSource();
-            newSource.buffer = bufferSource.buffer;
-            bufferSource = newSource;
-            setOnEnded();
-            bufferSource.connect(analyzer);
-            bufferSource.connect(gainNode);
-            bufferSource.connect(context.destination);
-            bufferSource.start(0, currentTime / 1000);
-            started = Date.now();
+        if (started) {
+            if (isPlaying) {
+                bufferSource.stop();
+                currentTime += Date.now() - started;
+                velMult = 0;
+            } else {
+                var newSource = context.createBufferSource();
+                newSource.buffer = bufferSource.buffer;
+                bufferSource = newSource;
+                setOnEnded();
+                bufferSource.connect(analyzer);
+                bufferSource.connect(gainNode);
+                bufferSource.connect(context.destination);
+                bufferSource.start(0, currentTime / 1000);
+                started = Date.now();
+            }
+            isPlaying = !isPlaying;
         }
-        isPlaying = !isPlaying;
     }
 });
 
